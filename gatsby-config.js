8489a352be9e0c48e498987f5e90cfd7f07b006f
AndyTCF <andy.c.f.98@gmail.com>
2019-09-25 22:55:21 +1000

    Add manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `ANDY TOM`,
+    title: `Andy Tom | Developer and Designer`,
     description: `A living and breathing portfolio for Andy Tom`,
     author: `Andy Tom`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-smoothscroll`,
     {
       resolve: `gatsby-plugin-layout`,
       options: {
@@ -20,8 +23,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Andy Tom's Portfolio`,
+        short_name: `ATCF Portfolio`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
